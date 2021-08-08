dfedfb20db1bcb4edeac80be1b7dc420b20ddfc6
SHIVANGI MEHTA <88172503+Shivangimeht@users.noreply.github.com>
2021-08-08 18:25:33 -0500

    Update cachematrix.R
    Completed

--- cachematrix.R
+++ cachematrix.R
@@ -3,23 +3,71 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makecachematix <- function(x = matrix()){
+  inv <- NULL 
+  set <- function(y){
+         x <<- y 
+         inv <<- NULL
+           }
+  get <- function() {x}
+  setInverse <- function(inverse) {inv <<- inverse}
+  getInverse <- function() {inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
 
+cachesolve <- function(x, ...){
+     inv <- x$getInverse()
+     if(!is.null(inv)){
+           message("getting cached data")
+     }
+     mat <- x$get()
+     inv <- solve(mat,...)
+     x$setInverse(inv)
+     inv
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
-makeCacheMatrix <- function(x = matrix()) {
 
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+source("C:/Users/Shivangi95/Desktop/makeCacheMatrix.R")
+> pmatrix <- makecachematix(matrix(1:16), nrow = 4, ncol = 4)
+Error in makecachematix(matrix(1:16), nrow = 4, ncol = 4) : 
+  unused arguments (nrow = 4, ncol = 4)
+> pmatrix <- makecachematix(matrix(1:16), nrow=4, ncol=4)
+Error in makecachematix(matrix(1:16), nrow = 4, ncol = 4) : 
+  unused arguments (nrow = 4, ncol = 4)
+> pmatirx$get()
+Error: object 'pmatirx' not found
+> pmatrix <- makecachematix(matrix(1:16, nrow=4, ncol=4))
+> pmatrix$get()
+     [,1] [,2] [,3] [,4]
+[1,]    1    5    9   13
+[2,]    2    6   10   14
+[3,]    3    7   11   15
+[4,]    4    8   12   16
+> pmatrix$getInverse()
+NULL
+> pmatrix <- nakeCacheMatrix(matrix(1:4, nrow=2, ncol=2))
+Error in nakeCacheMatrix(matrix(1:4, nrow = 2, ncol = 2)) : 
+  could not find function "nakeCacheMatrix"
+> pmatrix <- makeCacheMatrix(matrix(1:4, nrow=2,ncol=2))
+Error in makeCacheMatrix(matrix(1:4, nrow = 2, ncol = 2)) : 
+  could not find function "makeCacheMatrix"
+> pmatrix <- makecachematix(matrix(1:4, nrow=2, ncol=2))
+> pmatrix$get()
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> pmatrix$getInverse()
+NULL
+> cachesolve(pmatrix)
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> cacheSolve(pmatrix)
+Error in cacheSolve(pmatrix) : could not find function "cacheSolve"
+> pmatrix$getInverse()
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> 
